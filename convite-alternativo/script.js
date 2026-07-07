647f14fd88498c6614f8f2e5fb6ff27694eabbf1
Júlio <slimjuliodias@gmail.com>
2026-07-07 17:30:54 -0300

    Adjust canvas scale from 1.6 to 1.5

--- convite-alternativo/script.js
+++ convite-alternativo/script.js
@@ -126,7 +126,7 @@ btnImagem.addEventListener("click", () => {
     htmlElement.style.scrollBehavior = "auto";
 
     html2canvas(cartao, {
-        scale: 1.6,
+        scale: 1.5,
         backgroundColor: corDeFundo,
         useCORS: true,
         logging: false
