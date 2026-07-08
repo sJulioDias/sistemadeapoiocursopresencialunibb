1d14347f32adfb1ec8ebcdf5f7f5aa360940f987
Júlio <slimjuliodias@gmail.com>
2026-07-08 09:19:17 -0300

    Increase html2canvas scale from 1.5 to 2

--- convite-alternativo/script.js
+++ convite-alternativo/script.js
@@ -126,7 +126,7 @@ btnImagem.addEventListener("click", () => {
     htmlElement.style.scrollBehavior = "auto";
 
     html2canvas(cartao, {
-        scale: 1.5,
+        scale: 2,
         backgroundColor: corDeFundo,
         useCORS: true,
         logging: false
