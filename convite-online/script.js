d5da47b86f7c7297fcc9c3e986048f08a7d90b29
Júlio <slimjuliodias@gmail.com>
2026-07-09 23:43:13 -0300

    Update instructions with additional link and formatting

--- convite-online/script.js
+++ convite-online/script.js
@@ -118,9 +118,9 @@ ${sobre}
 
 Processo de inscrição obrigatório:
 1. Negocie sua liberação prévia com sua chefia imediata.
-2. Acesse a intranet através do Portal Capacita Aqui.
+2. Acesse a intranet através do Portal Capacita Aqui, https://gepesbhz.intranet.bb.com.br/capacitaaqui/
 3. Realize a inscrição oficial na ferramenta até o dia ${prazo}.
-4. Imprescindível: Peça para um gestor validar sua vaga acessando a aba "Gestor".
+4. Imprescindível: Peça para um gestor validar sua vaga acessando a aba "Gestor/a".
 
 Sua presença fará toda a diferença!
 
